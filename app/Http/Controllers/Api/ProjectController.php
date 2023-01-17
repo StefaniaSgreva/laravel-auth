43740cf18c1abcf9067db2acca9f4809fd4b44d3
StefaniaSgreva <stefaniasgreva@gmail.com>
2023-01-17 22:47:40 +0100

    added Api/ProjectController

---
+++ app/Http/Controllers/Api/ProjectController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Project;
+
+class ProjectController extends Controller
+{
+    public function index()
+    {
+        // $projects = Project::all();
+        // $projects = Project::paginate(5);
+        // $projects = Project::with('category', 'tags')->get(); //li prende tutti se non lo voglio paginato
+        $projects = Project::with('category', 'tags')->paginate(5); //di solito non si fa così per tutti -> si fa per il singolo projects, non si scaricano i dettagli per tutti i project in all
+        return response()->json([
+            'success' => true,
+            'result' => $projects //sto passando solo i project e non category e tag -solo category id-
+        ]);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\ProjectController;
 
 /*
 |--------------------------------------------------------------------------
@@ -11,9 +12,13 @@ use Illuminate\Support\Facades\Route;
 | Here is where you can register API routes for your application. These
 | routes are loaded by the RouteServiceProvider within a group which
 | is assigned the "api" middleware group. Enjoy building your API!
+/
+E' un gruppo già assegnato alle middleware
 |
 */
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('projects', [ProjectController::class, 'index']);
