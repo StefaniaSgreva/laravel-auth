40b0a2f9cb8e4c6044b3cac6b9ab2f0a5c582b4a
StefaniaSgreva <stefaniasgreva@gmail.com>
2023-01-18 00:25:29 +0100

    add route show api

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -19,4 +19,19 @@ class ProjectController extends Controller
             'result' => $projects //sto passando solo i project e non category e tag -solo category id-
         ]);
     }
+    public function show($slug)
+    {
+        $project = Project::with('category', 'tags')->where('slug', $slug)->first();
+        if ($project) {
+            return response()->json([
+                'success' => true,
+                'result' => $project
+            ]);
+        } else {
+            return response()->json([
+                'success' => false,
+                'result' => 'No Project found'
+            ]);
+        }
+    }
 }
